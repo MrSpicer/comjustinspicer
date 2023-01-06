99cb13a04c7b587ac6c784a4551b6fff203e95f8
Justin <Justin@justinspicer.com>
2023-01-05 22:57:24 -0600

    init some mocking

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace comjustinspicer.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
