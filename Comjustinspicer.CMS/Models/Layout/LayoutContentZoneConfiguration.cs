aacc36a39ded9385b09bfcc09545a7777c53918b
Justin Spicer <justin@justinspicer.com>
2026-02-05 01:12:28 -0600

    feat: Implement flexible layout system with multiple content zone configurations and new layout templates

---
+++ Comjustinspicer.CMS/Models/Layout/LayoutContentZoneConfiguration.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+using Comjustinspicer.CMS.Attributes;
+
+namespace Comjustinspicer.CMS.Models.Layout;
+
+/// <summary>
+/// Configuration model for the Layout ViewComponent when used within a content zone.
+/// </summary>
+public class LayoutContentZoneConfiguration
+{
+    /// <summary>
+    /// Gets or sets the name of the view to render.
+    /// </summary>
+    [ContentZoneProperty(
+        Label = "View Name",
+        HelpText = "Select the view template to render.",
+        EditorType = EditorType.ViewPicker,
+        ViewComponentName = "Layout",
+        IsRequired = true,
+        Order = 1
+    )]
+    [Required(ErrorMessage = "Please select a view.")]
+    public string ViewName { get; set; } = string.Empty;
+}
--- Comjustinspicer.CMS/ViewComponents/LayoutViewComponent.cs
+++ Comjustinspicer.CMS/ViewComponents/LayoutViewComponent.cs
@@ -1,18 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
-using Comjustinspicer.CMS.Data;
-using Comjustinspicer.Models;
-using System.Threading;
-using Comjustinspicer.CMS.Models.ContentZone;
+using Comjustinspicer.CMS.Attributes;
+using Comjustinspicer.CMS.Models.Layout;
 
 namespace Comjustinspicer.CMS.ViewComponents;
 
-//This is a work in progress to allow different layouts to be used for different pages.
-
+/// <summary>
+/// Renders a selected view template. Used for flexible layout/partial rendering in content zones.
+/// </summary>
+[ContentZoneComponent(
+    DisplayName = "Layout",
+    Description = "Renders a selected view template.",
+    Category = "General",
+    ConfigurationType = typeof(LayoutContentZoneConfiguration),
+    IconClass = "fa-columns",
+    Order = 10
+)]
 public class LayoutViewComponent : ViewComponent
 {
-	public async Task<IViewComponentResult> InvokeAsync(string layoutName)
-	{
-		await Task.Delay(0);
-		return View(layoutName);
-	}
+    /// <summary>
+    /// Renders the configured view.
+    /// </summary>
+    /// <param name="config">The content zone configuration.</param>
+    /// <returns>The view component result.</returns>
+    public IViewComponentResult Invoke(LayoutContentZoneConfiguration config)
+    {
+        config ??= new LayoutContentZoneConfiguration();
+        return View(config.ViewName);
+    }
 }
